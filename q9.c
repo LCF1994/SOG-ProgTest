7dd72210192b983933bb24d448854bd1a23ee0f8
Campos Ferreira <lucascampos_ferreira.ext@siemens.com>
2020-11-11 14:44:30 -0300

    alteração no tamanho do vetor v3 e inclusão de mais um numero a ser buscado nos testes de cada vetor

--- q9.c
+++ q9.c
@@ -40,7 +40,7 @@ void teste(int *v, int size_v, int chave){
 
 int main(){
     int v2[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    int v3[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int v3[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
     printf("Vetores de teste :\n");
     print_vetor("v2", v2, 10);
@@ -48,11 +48,13 @@ int main(){
     teste(v2, 10, 6); 
     teste(v2, 10, 2); 
     teste(v2, 10, 11);
+    teste(v2, 10, 13);
 
     printf("\nVetores de teste :\n");
     print_vetor("v3", v3, 10);
 
-    teste(v3, 10, 6); 
-    teste(v3, 10, 2); 
-    teste(v3, 10, 11);
+    teste(v3, 12, 6); 
+    teste(v3, 12, 2); 
+    teste(v3, 12, 11);
+    teste(v3, 12, 13);
 }
